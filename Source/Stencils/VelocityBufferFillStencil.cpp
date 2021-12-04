c7657326bcd5a3837d8d7596fcd2165358860d70
ge59nuy <e.mashayekh@tum.de>
2021-12-04 19:32:36 +0100

    fix the errors of velocity functions

--- Source/Stencils/VelocityBufferFillStencil.cpp
+++ Source/Stencils/VelocityBufferFillStencil.cpp
@@ -9,20 +9,20 @@ namespace NSEOF::Stencils {
      * Functions for 3D
      */
 
-    void PressureBufferReadStencil::applyLeftWall(FlowField& flowField, int i, int j, int k) {}
-    void PressureBufferReadStencil::applyRightWall(FlowField& flowField, int i, int j, int k) {}
-    void PressureBufferReadStencil::applyBottomWall(FlowField& flowField, int i, int j, int k) {}
-    void PressureBufferReadStencil::applyTopWall(FlowField& flowField, int i, int j, int k) {}
-    void PressureBufferReadStencil::applyFrontWall(FlowField& flowField, int i, int j, int k) {}
-    void PressureBufferReadStencil::applyBackWall(FlowField& flowField, int i, int j, int k) {}
+    void VelocityBufferFillStencil::applyLeftWall(FlowField& flowField, int i, int j, int k) {}
+    void VelocityBufferFillStencil::applyRightWall(FlowField& flowField, int i, int j, int k) {}
+    void VelocityBufferFillStencil::applyBottomWall(FlowField& flowField, int i, int j, int k) {}
+    void VelocityBufferFillStencil::applyTopWall(FlowField& flowField, int i, int j, int k) {}
+    void VelocityBufferFillStencil::applyFrontWall(FlowField& flowField, int i, int j, int k) {}
+    void VelocityBufferFillStencil::applyBackWall(FlowField& flowField, int i, int j, int k) {}
 
     /**
      * Functions for 2D
      */
 
-    void PressureBufferReadStencil::applyLeftWall(FlowField& flowField, int i, int j) {}
-    void PressureBufferReadStencil::applyRightWall(FlowField& flowField, int i, int j) {}
-    void PressureBufferReadStencil::applyBottomWall(FlowField& flowField, int i, int j) {}
-    void PressureBufferReadStencil::applyTopWall(FlowField& flowField, int i, int j) {}
+    void VelocityBufferFillStencil::applyLeftWall(FlowField& flowField, int i, int j) {}
+    void VelocityBufferFillStencil::applyRightWall(FlowField& flowField, int i, int j) {}
+    void VelocityBufferFillStencil::applyBottomWall(FlowField& flowField, int i, int j) {}
+    void VelocityBufferFillStencil::applyTopWall(FlowField& flowField, int i, int j) {}
 
 } // namespace NSEOF::Stencils
--- Source/Stencils/VelocityBufferReadStencil.cpp
+++ Source/Stencils/VelocityBufferReadStencil.cpp
@@ -9,20 +9,20 @@ namespace NSEOF::Stencils {
      * Functions for 3D
      */
 
-    void PressureBufferReadStencil::applyLeftWall(FlowField& flowField, int i, int j, int k) {}
-    void PressureBufferReadStencil::applyRightWall(FlowField& flowField, int i, int j, int k) {}
-    void PressureBufferReadStencil::applyBottomWall(FlowField& flowField, int i, int j, int k) {}
-    void PressureBufferReadStencil::applyTopWall(FlowField& flowField, int i, int j, int k) {}
-    void PressureBufferReadStencil::applyFrontWall(FlowField& flowField, int i, int j, int k) {}
-    void PressureBufferReadStencil::applyBackWall(FlowField& flowField, int i, int j, int k) {}
+    void VelocityBufferReadStencil::applyLeftWall(FlowField& flowField, int i, int j, int k) {}
+    void VelocityBufferReadStencil::applyRightWall(FlowField& flowField, int i, int j, int k) {}
+    void VelocityBufferReadStencil::applyBottomWall(FlowField& flowField, int i, int j, int k) {}
+    void VelocityBufferReadStencil::applyTopWall(FlowField& flowField, int i, int j, int k) {}
+    void VelocityBufferReadStencil::applyFrontWall(FlowField& flowField, int i, int j, int k) {}
+    void VelocityBufferReadStencil::applyBackWall(FlowField& flowField, int i, int j, int k) {}
 
     /**
      * Functions for 2D
      */
 
-    void PressureBufferReadStencil::applyLeftWall(FlowField& flowField, int i, int j) {}
-    void PressureBufferReadStencil::applyRightWall(FlowField& flowField, int i, int j) {}
-    void PressureBufferReadStencil::applyBottomWall(FlowField& flowField, int i, int j) {}
-    void PressureBufferReadStencil::applyTopWall(FlowField& flowField, int i, int j) {}
+    void VelocityBufferReadStencil::applyLeftWall(FlowField& flowField, int i, int j) {}
+    void VelocityBufferReadStencil::applyRightWall(FlowField& flowField, int i, int j) {}
+    void VelocityBufferReadStencil::applyBottomWall(FlowField& flowField, int i, int j) {}
+    void VelocityBufferReadStencil::applyTopWall(FlowField& flowField, int i, int j) {}
 
 } // namespace NSEOF::Stencils
