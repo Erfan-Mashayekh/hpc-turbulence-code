c9fc9cdb4e0411bac857e21a8523669968e077dc
Batuhan Erden <batuhan.erden@tum.de>
2022-01-17 01:06:03 +0100

    fix the indexing issue in the computation of the RHS

--- Source/Solvers/EigenSolver.cpp
+++ Source/Solvers/EigenSolver.cpp
@@ -205,15 +205,15 @@ namespace NSEOF::Solvers {
 
     void EigenSolver::computeRHS_() {
         if (parameters_.geometry.dim == 2) { // 2D
-            for (int i = 0; i < cellsX_; i++) {
-                for (int j = 0; j < cellsY_; j++) {
+            for (int i = 1; i < cellsX_; i++) {
+                for (int j = 1; j < cellsY_; j++) {
                     rhs_(COLUMN_MAJOR_IND(i, j, 0, cellsY_, cellsZ_)) = flowField_.getRHS().getScalar(i + 1, j + 1);
                 }
             }
         } else { // 3D
-            for (int i = 0; i < cellsX_; i++) {
-                for (int j = 0; j < cellsY_; j++) {
-                    for (int k = 0; k < cellsZ_; k++) {
+            for (int i = 1; i < cellsX_ - 1; i++) {
+                for (int j = 1; j < cellsY_ - 1; j++) {
+                    for (int k = 1; k < cellsZ_ - 1; k++) {
                         rhs_(COLUMN_MAJOR_IND(i, j, k, cellsY_, cellsZ_)) = flowField_.getRHS().getScalar(i + 1, j + 1, k + 1);
                     }
                 }
